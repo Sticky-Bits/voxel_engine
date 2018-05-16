b2482a910ab6bbd762b178ede1aa867ff90b9039
Dean Mckendry <dean.mckendry91@gmail.com>
2018-05-17 09:03:52 +1000

    Fix mesh generation

--- src/Chunk.hpp
+++ src/Chunk.hpp
@@ -11,7 +11,6 @@ class Chunk
 public:
 	Chunk(glm::vec3 chunk_position);
     void generate_voxels();
-    void generate_mesh_greedy_old();
     void generate_mesh_greedy();
 	void generate_mesh_dumb();
 	void load_mesh();
@@ -20,7 +19,7 @@ public:
 	void index_to_xyz(int index, int *x, int *y, int *z);
 
     std::vector<float> vertices;
-    std::vector<int> faces;
+    std::vector<int> indices;
     int voxels[CHUNK_SIZE_CUBED];
 	glm::vec3 chunk_position;
 	glm::vec3 world_position;
@@ -34,7 +33,7 @@ Chunk::Chunk(glm::vec3 position)
 	chunk_position = position;
 	world_position = chunk_position * (float)CHUNK_SIZE;
     generate_voxels();
-    generate_mesh_greedy();
+    generate_mesh_dumb();
 	load_mesh();
 }
 
@@ -49,7 +48,7 @@ void Chunk::generate_voxels()
 	for (int y = 0; y < CHUNK_SIZE; y++)
 	for (int z = 0; z < CHUNK_SIZE; z++, i++)
     {
-		if (y<2) {
+		if (y<20) {
 			voxels[i] = 1;
 		} else {
 			voxels[i] = 0;
@@ -58,7 +57,8 @@ void Chunk::generate_voxels()
 }
 
 int Chunk::xyz_to_index(int x, int y, int z){
-	return (z * CHUNK_SIZE_SQUARED) + (y * CHUNK_SIZE) + (z * CHUNK_SIZE);
+	// return (x * CHUNK_SIZE_SQUARED) + (y * CHUNK_SIZE) + z;
+	return z + CHUNK_SIZE * (y + CHUNK_SIZE * x);
 }
 
 void Chunk::index_to_xyz(int index, int *x, int *y, int *z){
@@ -79,17 +79,17 @@ void Chunk::generate_mesh_greedy()
         q[d] = 1;
         for (x[d] = -1; x[d] < CHUNK_SIZE; )
         {
-            // Compute mask. mask contains ints referring to voxel positions to render
+            // Compute mask
             int n = 0;
             for (x[v] = 0; x[v] < CHUNK_SIZE; ++x[v])
             {
                 for (x[u] = 0; x[u] < CHUNK_SIZE; ++x[u], ++n)
                 {
-					int a = (0    <= x[d]          ? xyz_to_index(x[0], x[1], x[2])                : 0);
-                    int b = (x[d] < CHUNK_SIZE - 1 ? xyz_to_index(x[0]+q[0], x[1]+q[1], x[2]+q[2]) : 0);
-					if((!!a) == (!!b)) { // If c++ breaks on this, it's just int->bool casting
+					int a = (0    <= x[d]          ? voxels[xyz_to_index(x[0], x[1], x[2])]                : 0);
+                    int b = (x[d] < CHUNK_SIZE - 1 ? voxels[xyz_to_index(x[0]+q[0], x[1]+q[1], x[2]+q[2])] : 0);
+					if(((bool)a) == ((bool)b)) {
 						mask[n] = 0;
-					} else if(!!a) {
+					} else if((bool)a) {
 						mask[n] = a;
 					} else {
 						mask[n] = -b;
@@ -104,7 +104,7 @@ void Chunk::generate_mesh_greedy()
                 for (i = 0; i < CHUNK_SIZE; )
                 {
 					int c = mask[n];
-                    if(!!c)
+                    if((bool)c)
                     {
                         //Compute width
                         for (w = 1; c == mask[n+w] && i+w < CHUNK_SIZE; ++w)
@@ -159,15 +159,15 @@ void Chunk::generate_mesh_greedy()
 						vertices.push_back(x[1] + du[1] + dv[1]);//y3
 						vertices.push_back(x[2] + du[2] + dv[2]);//z3
 
-						// // I think faces are indices
-						// faces.push_back(vertex_count);
-						// faces.push_back(vertex_count+1);
-						// faces.push_back(vertex_count+2);
-						// // faces.push_back(c); // Might be color?
-						// faces.push_back(vertex_count);
-						// faces.push_back(vertex_count+2);
-						// faces.push_back(vertex_count+3);
-						// // faces.push_back(c); // Might be color?
+						// // push indicies. There will be some overlap here, but that's ok.
+						// indices.push_back(vertex_count);
+						// indices.push_back(vertex_count+1);
+						// indices.push_back(vertex_count+2);
+						// // indices.push_back(c); // Might be color?
+						// indices.push_back(vertex_count);
+						// indices.push_back(vertex_count+2);
+						// indices.push_back(vertex_count+3);
+						// // indices.push_back(c); // Might be color?
 
 						// TODO: Check correct winding order for face culling
                         // Zero out mask
@@ -189,165 +189,14 @@ void Chunk::generate_mesh_greedy()
 	}
 }
 
-void Chunk::generate_mesh_greedy_old()
-{
-    // Sweep over 3 axes
-    for (int d = 0; d < 3; d++)
-    {
-        int i, j, k, l, w, h
-        , u = (d+1)%3
-        , v = (d+2)%3
-        , x[] = {0,0,0}
-        , q[] = {0,0,0};
-		// Crude Normals
-		float normalx, normaly, normalz;
-		// d0 = x, d1 = y, d2 = z
-		if (d == 0)
-		{
-			normalx = 1.0f;
-			normaly = 0.0f;
-			normalz = 0.0f;
-		}
-		else if (d == 1)
-		{
-			normalx = 0.0f;
-			normaly = 1.0f;
-			normalz = 0.0f;
-		}
-		else // d == 2
-		{
-			normalx = 0.0f;
-			normaly = 0.0f;
-			normalz = 1.0f;
-		}
-        bool mask[CHUNK_SIZE_SQUARED] = {false};
-        q[d] = 1;
-        for (x[d] = -1; x[d] < CHUNK_SIZE; )
-        {
-            // Compute mask
-            int n = 0;
-            for (x[v] = 0; x[v] < CHUNK_SIZE; ++x[v])
-            {
-                for (x[u] = 0; x[u] < CHUNK_SIZE; ++x[u])
-                {
-                    mask[n++] = 
-                        (0    <= x[d]          ? voxels[xyz_to_index(x[0], x[1], x[2])]                : false) !=
-                        (x[d] < CHUNK_SIZE - 1 ? voxels[xyz_to_index(x[0]+q[0], x[1]+q[1], x[2]+q[2])] : false);
-                }
-            }
-            ++x[d];
-            // Generate mesh for mask using lexicographic ordering
-            n = 0;
-            for (j = 0; j < CHUNK_SIZE; ++j)
-            {
-                for (i = 0; i < CHUNK_SIZE; )
-                {
-                    if(mask[n])
-                    {
-                        //Compute width
-                        for (w = 1; mask[n+w] && i+w < CHUNK_SIZE; ++w)
-                        {}
-                        //Compute height
-                        bool done = false;
-                        for (h = 1; j+h < CHUNK_SIZE; ++h)
-                        {
-                            for (k = 0; k < w; ++k)
-                            {
-                                if (!mask[n+k+h*CHUNK_SIZE])
-                                {
-                                    done = true;
-                                    break;
-                                }
-                            }
-                            if (done)
-                            {
-                                break;
-                            }
-                        }
-                        // Add vertex data
-						x[u] = i; x[v] = j;
-						int du[] = { 0,0,0 }; du[u] = w;
-						int dv[] = { 0,0,0 }; dv[v] = h;
-						vertices.push_back(x[0]);//x
-						vertices.push_back(x[1]);//y
-						vertices.push_back(x[2]);//z
-						vertices.push_back(normalx);
-						vertices.push_back(normaly);
-						vertices.push_back(normalz);
-						vertices.push_back(x[0]);//texture x
-						vertices.push_back(x[1]);//texture y
-
-						vertices.push_back(x[0] + du[0]);
-						vertices.push_back(x[1] + du[1]);
-						vertices.push_back(x[2] + du[2]);
-						vertices.push_back(normalx);
-						vertices.push_back(normaly);
-						vertices.push_back(normalz);
-						vertices.push_back(x[0] + du[0]);//texture x
-						vertices.push_back(x[1] + du[1]);//texture y
-
-						vertices.push_back(x[0] + du[0] + dv[0]);
-						vertices.push_back(x[1] + du[1] + dv[1]);
-						vertices.push_back(x[2] + du[2] + dv[2]);
-						vertices.push_back(normalx);
-						vertices.push_back(normaly);
-						vertices.push_back(normalz);
-						vertices.push_back(x[0] + du[0] + dv[0]);//texture x
-						vertices.push_back(x[1] + du[1] + dv[1]);//texture y
-
-						vertices.push_back(x[0] + du[0] + dv[0]);
-						vertices.push_back(x[1] + du[1] + dv[1]);
-						vertices.push_back(x[2] + du[2] + dv[2]);
-						vertices.push_back(normalx);
-						vertices.push_back(normaly);
-						vertices.push_back(normalz);
-						vertices.push_back(x[0] + du[0] + dv[0]);//texture x
-						vertices.push_back(x[1] + du[1] + dv[1]);//texture y
-
-						vertices.push_back(x[0] + dv[0]);
-						vertices.push_back(x[1] + dv[1]);
-						vertices.push_back(x[2] + dv[2]);
-						vertices.push_back(normalx);
-						vertices.push_back(normaly);
-						vertices.push_back(normalz);
-						vertices.push_back(x[0] + dv[0]);//texture x
-						vertices.push_back(x[1] + dv[1]);//texture y
-
-						vertices.push_back(x[0]);//x
-						vertices.push_back(x[1]);//y
-						vertices.push_back(x[2]);//z
-						vertices.push_back(normalx);
-						vertices.push_back(normaly);
-						vertices.push_back(normalz);
-						vertices.push_back(x[0]);//texture x
-						vertices.push_back(x[1]);//texture y
-						// TODO: Check correct winding order for face culling
-                        // Zero out mask
-                        for (l=0; l<h; ++l)
-                        {
-                            for (k=0; k<w; ++k)
-                            {
-                                mask[n+k+l*CHUNK_SIZE] = false;
-                            }
-                        }
-                        //Increment counters and continue
-                        i += w; n += w;
-                    } else {
-                        ++i; ++n;
-                    }
-                }
-            }
-        }
-    }
-}
-
 void Chunk::generate_mesh_dumb(){
 	// Define a dumb mesh generator. Might be useful for testing
-	int x[] = {0,0,0};
+	// Doesn't actually work atm :(
+	int x[] = {0,0,0}; int n = 0;
 	for (x[2]=0; x[2]<CHUNK_SIZE; x[2]++){
 		for (x[1]=0; x[1]<CHUNK_SIZE; x[1]++){
-			for (x[0]=0; x[0]<CHUNK_SIZE; x[0]++){
-				// if (voxels[xyz_to_index(x[2],x[1],x[0])]){
+			for (x[0]=0; x[0]<CHUNK_SIZE; x[0]++, n++){
+				if (voxels[n]){
 					for (int d=0; d<3; d++){
 						int t[] = {x[0],x[1],x[2]};
 						int u[] = {0,0,0};
@@ -356,6 +205,11 @@ void Chunk::generate_mesh_dumb(){
 						v[(d+2)%3] = 1;
 						for (int s=0; s<2; s++){
 							t[d] = x[d] + s;
+							// There must be a better way to do this...
+							int tmp[] = {0,0,0};
+							tmp[0] = u[0]; tmp[1] = u[1]; tmp[2] = u[2];
+							u[0] = v[0]; u[1] = v[1]; u[2] = v[2];
+							v[0] = tmp[0]; v[1] = tmp[1]; v[2] = tmp[2];
 							vertices.push_back(t[0]);
 							vertices.push_back(t[1]);
 							vertices.push_back(t[2]);
@@ -372,16 +226,16 @@ void Chunk::generate_mesh_dumb(){
 							vertices.push_back(t[1]);
 							vertices.push_back(t[2]);
 
-							vertices.push_back(t[0]+v[0]);
-							vertices.push_back(t[1]+v[1]);
-							vertices.push_back(t[2]+v[2]);
-
 							vertices.push_back(t[0]+u[0]+v[0]);
 							vertices.push_back(t[1]+u[1]+v[1]);
 							vertices.push_back(t[2]+u[2]+v[2]);
+
+							vertices.push_back(t[0]+v[0]);
+							vertices.push_back(t[1]+v[1]);
+							vertices.push_back(t[2]+v[2]);
 						}
 					}
-				// }
+				}
 			}
 		}
 	}
@@ -398,7 +252,7 @@ void Chunk::load_mesh(){
 	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), &vertices[0], GL_STATIC_DRAW);
 
 	// glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	// glBufferData(GL_ELEMENT_ARRAY_BUFFER, faces.size() * sizeof(int), &faces[0], GL_STATIC_DRAW);
+	// glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(int), &indices[0], GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
